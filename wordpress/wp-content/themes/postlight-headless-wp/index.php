f8ad95d280473ff300803ced7b783aef781f2e9a
Gina Trapani <ginatrapani@gmail.com>
2017-09-08 15:25:32 -0400

    fix: redirect posts to their API endpoints Closes #1

--- wordpress/wp-content/themes/postlight-headless-wp/index.php
+++ wordpress/wp-content/themes/postlight-headless-wp/index.php
@@ -1,2 +1,7 @@
 <?php
-header( 'Location: /wp-json/' );
+// Redirect individual post to the REST API endpoint
+if ( is_single() ) {
+	header( 'Location: /wp-json/wp/v2/posts/' . get_post()->ID );
+} else {
+	header( 'Location: /wp-json/' );
+}
