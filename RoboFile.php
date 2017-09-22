b2b97b0ea184ac8accbbdb415a2bc2a58b57b328
Gina Trapani <ginatrapani@gmail.com>
2017-09-22 18:05:34 -0400

    chore: upgrade to WordPress 4.8.2

--- RoboFile.php
+++ RoboFile.php
@@ -32,7 +32,7 @@ class RoboFile extends \Robo\Tasks {
 		. ' . * to ' . $opts['wp-db-name'] . "@localhost identified by '" . $opts['wp-db-name'] . "'\"" );
 		$this->_exec( "mysql -uroot -e 'flush privileges'" );
 
-		$this->wp( 'core download --version=4.8.1 --force' );
+		$this->wp( 'core download --version=4.8.2 --force' );
 		$this->wp( 'core config --dbname=' . $opts['wp-db-name'] . ' --dbuser=' . $opts['wp-db-name'] . ' --dbpass=' . $opts['wp-db-name'] . ' --dbhost=127.0.0.1' );
 		$this->wp( 'db drop --yes' );
 		$this->wp( 'db create --yes' );
