ed841067b8f7e35af3e0e28fc6fc53cf069c8b07
Gina Trapani <ginatrapani@gmail.com>
2017-09-22 18:10:00 -0400

    feat: avoid CORS errors

--- wordpress/wp-content/themes/postlight-headless-wp/functions.php
+++ wordpress/wp-content/themes/postlight-headless-wp/functions.php
@@ -6,4 +6,7 @@ require_once( 'inc/class-acf-commands.php' );
 // Logging functions
 require_once( 'inc/log.php' );
 
+// CORS handling
+require_once( 'inc/cors.php' );
+
 // Add custom API endpoints here
---
+++ wordpress/wp-content/themes/postlight-headless-wp/inc/cors.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Allow GET requests from * origin
+ * Thanks to https://joshpress.net/access-control-headers-for-the-wordpress-rest-api/
+ */
+add_action( 'rest_api_init', function() {
+
+	remove_filter( 'rest_pre_serve_request', 'rest_send_cors_headers' );
+
+	add_filter( 'rest_pre_serve_request', function( $value ) {
+		header( 'Access-Control-Allow-Origin: *' );
+		header( 'Access-Control-Allow-Methods: GET' );
+		header( 'Access-Control-Allow-Credentials: true' );
+		return $value;
+	});
+}, 15 );
+
