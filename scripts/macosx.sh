3730e6a72b985e5bf255505a877965741863edee
George Haddad <george.haddad@postlight.com>
2017-10-11 00:22:38 +0300

    feat: use scripts directory and move macosx commands to script file

---
+++ scripts/macosx.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Download and install wp-cli
+brew install homebrew/php/wp-cli
+
+# Download and install robo
+brew install homebrew/php/robo
+
+# Install mysql-server
+brew install mysql
+
+# Start mysql-server
+mysql.server start
