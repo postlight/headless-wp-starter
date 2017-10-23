dc80e43e848423428af0580667b5b0c578250493
Gina Trapani <ginatrapani@gmail.com>
2017-10-23 14:11:24 -0400

    chore: consolidate install scripts into one

--- install.sh
+++ install.sh
@@ -1,11 +1,33 @@
 #!/bin/bash
 
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
-        chmod +x ./scripts/ubuntu.sh
-        ./scripts/ubuntu.sh
+    # Install mysql-server and preset root password
+    sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password root'
+    sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password root'
+    sudo apt-get -y install mysql-server
+    # Install PHP and PHP MySQL Plugin
+    sudo apt-get -y install php php-mysql
+    # Download and install wp-cli
+    wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+    sudo chmod +x wp-cli.phar
+    sudo mv wp-cli.phar /usr/local/bin/wp
+    # Download and install robo
+    wget http://robo.li/robo.phar
+    sudo chmod +x robo.phar
+    sudo mv robo.phar /usr/bin/robo
+    # Start MySQL Server
+    sudo service mysql start
 elif [[ "$OSTYPE" == "darwin"* ]]; then
-        chmod +x ./scripts/macosx.sh
-        ./scripts/macosx.sh
+    # Download and install wp-cli
+    brew install homebrew/php/wp-cli
+    # Download and install robo
+    brew install homebrew/php/robo
+    # Install mysql-server
+    brew install mysql
+    # Start mysql-server
+    mysql.server start
+    # Set the password to root to be compatible with the world
+    mysqladmin -u root password root
 else
-        echo "Unhandled OS type"
+    echo "Sorry, this installation script only works on Mac OS X and Ubuntu Linux. Looks like your operating system is $OSTYPE."
 fi
