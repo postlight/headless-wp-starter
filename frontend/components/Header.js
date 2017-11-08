9adffe25a71b844ebdcc4ad973e0c639454b6e80
Gina Trapani <ginatrapani@gmail.com>
2017-11-08 17:37:30 -0500

    feat: implement WordPress menu

--- frontend/components/Header.js
+++ frontend/components/Header.js
@@ -1,23 +1,60 @@
+import React, { Component } from "react";
 import Link from "next/link";
 
 const linkStyle = {
     marginRight: 15
 };
 
-// @TODO Use REST-powered menu http://localhost:8080/wp-json/menus/v1/menus/main
-const Header = () => (
-    <div>
-        <div>
-            <Link href="/">
-                <a style={linkStyle}>Home</a>
-            </Link>
-            <Link href="/about">
-                <a style={linkStyle}>About</a>
-            </Link>
-        </div>
-        <img src="static/images/wordpress-plus-react-header.png" width="1024" />
-        <h1>Hello Headless WordPress</h1>
-    </div>
-);
+class Header extends Component {
+    constructor() {
+        super();
+        this.state = {
+            menu: []
+        };
+    }
+    componentDidMount() {
+        const menuItemsURL =
+            "http://localhost:8080/wp-json/menus/v1/menus/main";
+        fetch(menuItemsURL)
+            .then(res => res.json())
+            .then(res => {
+                this.setState({
+                    menu: res.items
+                });
+            });
+    }
+
+    render() {
+        const menuItems = this.state.menu.map((item, index) => {
+            return (
+                <Link
+                    href={item.url.replace("http://localhost:8080", "")}
+                    key={item.ID}
+                >
+                    <a style={linkStyle}>{item.title}</a>
+                </Link>
+            );
+        });
+        return (
+            <div>
+                <div>
+                    <Link href="/">
+                        <a style={linkStyle}>Home</a>
+                    </Link>
+                    <Link href="/another-page">
+                        <a style={linkStyle}>Another Page</a>
+                    </Link>
+                    <span style={linkStyle}>WordPress menu:</span>
+                    {menuItems}
+                </div>
+                <img
+                    src="static/images/wordpress-plus-react-header.png"
+                    width="1024"
+                />
+                <h1>Hello Headless WordPress</h1>
+            </div>
+        );
+    }
+}
 
 export default Header;
