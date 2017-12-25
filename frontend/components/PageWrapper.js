65a677944896f3d0b10a5d026947068368663f4d
Tyler Barnes <tyler@bare.ca>
2017-12-25 01:03:57 -0800

    Add page wrapper

---
+++ frontend/components/PageWrapper.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { Config } from "../config.js";
+
+const PageWrapper = Comp => (
+  class extends React.Component {
+    static async getInitialProps(args) {
+      const headerMenuRes = await fetch(
+        `${Config.apiUrl}/wp-json/menus/v1/menus/header-menu`
+      );
+      const headerMenu = await headerMenuRes.json();
+      return {
+        headerMenu,
+        ...(Comp.getInitialProps ? await Comp.getInitialProps(args) : null),
+      };
+    }
+
+    render() {
+      return (
+        <Comp {...this.props} />
+      )
+    }
+  }
+)
+
+export default PageWrapper;
