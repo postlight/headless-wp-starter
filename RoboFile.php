158febf8763f017dac40c609bbb4307467afe433
Alexi Akl <alexiakl@gmail.com>
2018-04-12 15:55:56 +0300

    feat: ask for db root pass
    feat: ask for IP
    
    fix: friendlier prompt copy

--- RoboFile.php
+++ RoboFile.php
@@ -27,14 +27,26 @@ class RoboFile extends \Robo\Tasks {
 			return;
 		}
 
-		$this->_exec( "mysql -uroot -proot -h 0.0.0.0 -e 'create user if not exists " . $opts['wp-db-name'] . "'" );
-		$this->_exec( "mysql -uroot -proot -h 0.0.0.0 -e 'create database if not exists " . $opts['wp-db-name'] . "'" );
-		$this->_exec( 'mysql -uroot -proot -h 0.0.0.0 -e "grant all privileges on ' . $opts['wp-db-name']
-		. ' . * to ' . $opts['wp-db-name'] . "@localhost identified by '" . $opts['wp-db-name'] . "'\"" );
-		$this->_exec( "mysql -uroot -proot -h 0.0.0.0 -e 'flush privileges'" );
+		$dbpass = $this->ask("Database root password (press Enter for default value [root]): ");
+		if ( !$dbpass || strlen( $dbpass ) == 0 ) {
+			$dbpass = "root";
+		}
 
-		$this->wp( 'core download --version=4.9.5 --locale=en_US --force' );
-		$this->wp( 'core config --dbname=' . $opts['wp-db-name'] . ' --dbuser=' . $opts['wp-db-name'] . ' --dbpass=' . $opts['wp-db-name'] . ' --dbhost=0.0.0.0' );
+		$dbip = $this->ask("Database IP address (press Enter for default value [0.0.0.0]): ");
+		if ( !$dbip || strlen( $dbip ) == 0 ) {
+			$dbip = "0.0.0.0";
+		}
+
+		$this->_exec( "mysql -uroot -p" . $dbpass . " -h " . $dbip . " -e 'create user if not exists " . $opts['wp-db-name'] . "'" );
+		$this->_exec( "mysql -uroot -p" . $dbpass . " -h " . $dbip . " -e 'create database if not exists " . $opts['wp-db-name'] . "'" );
+		$this->_exec( "mysql -uroot -p" . $dbpass . " -h " . $dbip . " -e \"grant all privileges on " . $opts['wp-db-name']
+		. ".* to " . $opts['wp-db-name'] . "@localhost identified by '" . $opts['wp-db-name'] . "'\"" );
+
+		$this->_exec( "mysql -uroot -p" . $dbpass . " -h " . $dbip . " -e 'flush privileges'" );
+
+		$this->wp( 'core download --version=4.9.4 --locale=en_US --force' );
+		$this->wp( 'core config --dbname=' . $opts['wp-db-name'] . ' --dbuser=' . $opts['wp-db-name'] . ' --dbpass=' . $opts['wp-db-name'] . ' --dbhost=' . $dbip );
+
 		$this->wp( 'db drop --yes' );
 		$this->wp( 'db create' );
 
