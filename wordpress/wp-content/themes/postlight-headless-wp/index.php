45e499c4a55aa9f882ba5691a8f6b7b4349a821c
Ryan Williams <ryan.williams@postlight.com>
2018-10-02 11:44:52 -0400

    fix: use post type rest base instead of slug for redirect

--- wordpress/wp-content/themes/postlight-headless-wp/index.php
+++ wordpress/wp-content/themes/postlight-headless-wp/index.php
@@ -1,7 +1,7 @@
 <?php
 // Redirect individual posts to the REST API endpoint
 if ( is_single() ) {
-    header( sprintf( 'Location: /wp-json/wp/v2/%s/%s', get_post_type(), get_post()->ID ) );
+    header( sprintf( 'Location: /wp-json/wp/v2/%s/%s', get_post_type_object( get_post_type() )->rest_base, get_post()->ID ) );
 } else {
     header( 'Location: /wp-json/' );
 }
