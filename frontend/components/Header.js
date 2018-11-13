62189b50ea5771f2acb6c0fea18ad7fee1d5cc88
Mark Neuburger <markneub@gmail.com>
2018-11-13 15:24:32 -0500

    Update page title and indentation

--- frontend/components/Header.js
+++ frontend/components/Header.js
@@ -6,28 +6,28 @@ import { Config } from "../config.js";
 import stylesheet from '../src/styles/style.scss'
 
 class Header extends Component {
-    constructor() {
-        super();
-    }
+  constructor() {
+    super();
+  }
 
-    render() {
+  render() {
 
-        return (
-            <div>
-                <Head>
-                    <style dangerouslySetInnerHTML={{ __html: stylesheet }} />
-                    <meta
-                        name="viewport"
-                        content="width=device-width, initial-scale=1"
-                    />
-                    <meta charSet="utf-8" />
-                    <title>
-                        WordPress + React Starter Kit Frontend by Postlight
-                    </title>
-                </Head>
-            </div>
-        );
-    }
+    return (
+      <div>
+        <Head>
+          <style dangerouslySetInnerHTML={{ __html: stylesheet }} />
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1"
+          />
+          <meta charSet="utf-8" />
+          <title>
+            Meredith Monk
+          </title>
+        </Head>
+      </div>
+    );
+  }
 }
 
 export default Header;
