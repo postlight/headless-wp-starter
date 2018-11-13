6ced46094eea6aa66b9ca5a5126dbceb9893405a
Mark Neuburger <markneub@gmail.com>
2018-11-13 15:21:17 -0500

    Fix indentation

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -7,27 +7,27 @@ import Menu from "../components/Menu.js";
 import { Config } from "../config.js";
 
 class Index extends Component {
-    static async getInitialProps(context) {
-        const pageRes = await fetch(
-            `${Config.apiUrl}/wp-json/postlight/v1/page?slug=welcome`
-        );
-        const page = await pageRes.json();
-        return { page };
-    }
+  static async getInitialProps(context) {
+    const pageRes = await fetch(
+      `${Config.apiUrl}/wp-json/postlight/v1/page?slug=welcome`
+    );
+    const page = await pageRes.json();
+    return { page };
+  }
 
-    render() {
-       return (
-            <Layout>
-                <Menu menu={this.props.headerMenu} />
-                <h1>{this.props.page.title.rendered}</h1>
-                <div
-                    dangerouslySetInnerHTML={{
-                        __html: this.props.page.content.rendered
-                    }}
-                />
-            </Layout>
-        );
-    }
+  render() {
+    return (
+      <Layout>
+        <Menu menu={this.props.headerMenu} />
+        <h1>{this.props.page.title.rendered}</h1>
+        <div
+          dangerouslySetInnerHTML={{
+            __html: this.props.page.content.rendered
+          }}
+        />
+      </Layout>
+    );
+  }
 }
 
 export default PageWrapper(Index);
