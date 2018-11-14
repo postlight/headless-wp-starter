f407a160b2733442fb605eeb6bf404cdaab64bb9
Mark Neuburger <markneub@gmail.com>
2018-11-14 16:12:21 -0500

    WIP

--- frontend/pages/post.js
+++ frontend/pages/post.js
@@ -1,10 +1,12 @@
 import Layout from "../components/Layout.js";
 import React, { Component } from "react";
+import Link from "next/link";
 import fetch from "isomorphic-unfetch";
 import Error from "next/error";
 import PageWrapper from "../components/PageWrapper.js";
 import Menu from "../components/Menu.js";
 import { Config } from "../config.js";
+import { createLink } from "../src/util.js";
 
 class Post extends Component {
   static async getInitialProps(context) {
@@ -13,15 +15,27 @@ class Post extends Component {
       `${Config.apiUrl}/wp-json/postlight/v1/${apiRoute}?slug=${slug}`
     );
     const post = await res.json();
-    return { post };
+    const childrenRes = await fetch(
+      `${Config.apiUrl}/wp-json/wp/v2/pages?parent=${post.id}`
+    );
+    const children = await childrenRes.json();
+    return { post, children };
   }
 
   render() {
-    if (!this.props.post.title) return <Error statusCode={404} />;
+    let { post, children } = this.props
+    if (!post.title) return <Error statusCode={404} />;
 
     return (
       <Layout>
         <Menu menu={this.props.headerMenu} />
+        { !!children.length &&
+          <ul id="sub-nav">
+            { children.map(createLink).map((child, i) =>
+              <li key={i}>{child}</li>
+            )}
+          </ul>
+        }
         <div
           dangerouslySetInnerHTML={{
             __html: this.props.post.content.rendered
