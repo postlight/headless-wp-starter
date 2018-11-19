1e30fccbcefe22d972248bf573460bf79634685d
Mark Neuburger <markneub@gmail.com>
2018-11-19 17:25:31 -0500

    Sort menu items by menu_order parameter

--- frontend/pages/page.js
+++ frontend/pages/page.js
@@ -8,6 +8,7 @@ import Menu from "../components/Menu.js";
 import { Config } from "../config.js";
 import { createLink } from "../src/util.js";
 import CalendarEvents from "../components/CalendarEvents.js";
+import sortBy from 'lodash/sortBy';
 
 class Post extends Component {
   static async getInitialProps(context) {
@@ -47,8 +48,10 @@ class Post extends Component {
             { !!menuItems.length &&
               <div className="col-3" id="subnav">              
                 <ul id="sub-nav">
-                  { menuItems.map(createLink).map((child, i) =>
-                    <li key={i}>{child}</li>
+                  { sortBy(menuItems, 'menu_order')
+                    .map(createLink)
+                    .map((child, i) =>
+                      <li key={i}>{child}</li>
                   )}
                 </ul>
               </div>
