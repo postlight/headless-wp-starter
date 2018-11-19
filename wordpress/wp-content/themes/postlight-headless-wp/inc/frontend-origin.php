3aefb237fe7728a45e48ec33d7530f70504f5c7c
Patrick Steadman <ptsteadman@gmail.com>
2018-11-19 16:25:52 -0500

    fix CORS issue by setting frontend url

--- wordpress/wp-content/themes/postlight-headless-wp/inc/frontend-origin.php
+++ wordpress/wp-content/themes/postlight-headless-wp/inc/frontend-origin.php
@@ -1,12 +1,8 @@
 <?php
 
 /**
- * Placeholder function for determining the frontend origin.
- *
- * @TODO Determine the headless client's URL based on the current environment.
- *
  * @return str Frontend origin URL, i.e., http://localhost:3000.
  */
 function get_frontend_origin() {
-    return 'http://localhost:3000';
+    return ($_ENV['FRONTEND_URL'] ? $_ENV['FRONTEND_URL'] : 'http://localhost:3000');
 }
