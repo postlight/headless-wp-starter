20ab070317874c184b38815f68876a27aaf948fc
Patrick Steadman <ptsteadman@gmail.com>
2018-11-19 17:57:24 -0500

    hardcode prod api url for now

--- frontend/config.js
+++ frontend/config.js
@@ -1,3 +1,5 @@
 export const Config = {
-  apiUrl: process.env.WORDPRESS_URL || 'http://localhost:8080'
+  apiUrl: process.env.NODE_ENV == 'production'
+    ? 'https://wordpress-dot-wordpress-test-222422.appspot.com'
+    : 'http://localhost:8080'
 }
