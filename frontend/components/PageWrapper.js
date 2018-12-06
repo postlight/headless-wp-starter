0cfd039f0610b4183dd759d191ecdfd1c908f33d
Mark Neuburger <markneub@gmail.com>
2018-12-05 19:49:42 -0500

    Remove unnecessary API calls

--- frontend/components/PageWrapper.js
+++ frontend/components/PageWrapper.js
@@ -3,26 +3,39 @@ import { Config } from "../config.js";
 
 const PageWrapper = Comp => (
   class extends React.Component {
+    constructor(props) {
+      super(props)
+      this.state = this.state || {}
+      this.state.headerMenu = this.props.headerMenu
+      this.state.repertoryWorks = this.props.repertoryWorks
+    }
+
     static async getInitialProps(args) {
-      const headerMenuRes = await fetch(
-        `${Config.apiUrl}/wp-json/menus/v1/menus/header-menu`
-      );
-      const repertoryWorksRes = await fetch(
-        `${Config.apiUrl}/wp-json/wp/v2/work?_embed`
-      );
-      const headerMenu = await headerMenuRes.json();
-      const repertoryWorks = await repertoryWorksRes.json();
+      if (!!args.req) {
+        const headerMenuRes = await fetch(
+          `${Config.apiUrl}/wp-json/menus/v1/menus/header-menu`
+        );
+        const repertoryWorksRes = await fetch(
+          `${Config.apiUrl}/wp-json/wp/v2/work?_embed`
+        );
+        const headerMenu = await headerMenuRes.json();
+        const repertoryWorks = await repertoryWorksRes.json();
+
+        return {
+          headerMenu,
+          repertoryWorks,
+          ...(Comp.getInitialProps ? await Comp.getInitialProps(args) : null),
+        };
+      }
       return {
-        headerMenu,
-        repertoryWorks,
-        ...(Comp.getInitialProps ? await Comp.getInitialProps(args) : null),
-      };
+        ...(Comp.getInitialProps ? await Comp.getInitialProps(args) : null)
+      }
     }
 
     render() {
       return (
         <div id="wrapper">
-          <Comp {...this.props} />
+          <Comp {...{...this.props, ...this.state}} />
         </div>
       )
     }
