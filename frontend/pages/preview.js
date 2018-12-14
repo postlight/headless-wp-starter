bc3e661a7b2918893d1929be962ed6bdfd05b146
Ryan Williams <ryan.williams@postlight.com>
2018-12-14 15:40:11 -0500

    fix: restore fetch for preview
    (until wpapi supports oauth)

--- frontend/pages/preview.js
+++ frontend/pages/preview.js
@@ -1,29 +1,29 @@
 import Layout from "../components/Layout.js";
 import React, { Component } from "react";
+import fetch from "isomorphic-unfetch";
 import Error from "next/error";
 import PageWrapper from "../components/PageWrapper.js";
 import Menu from "../components/Menu.js";
 import { Config } from "../config.js";
-import WPAPI from "wpapi";
-
-const wp = new WPAPI({
-    endpoint: Config.apiUrl,
-    username: Config.apiUsername,
-    password: Config.apiPassword,
-});
 
 class Preview extends Component {
-    constructor(props) {
-        super(props);
-
-        const { id, wpnonce } = this.props.url.query;
-
+    constructor() {
+        super();
         this.state = {
             post: null
         };
+    }
 
-        wp.posts().id(id)
-            .then((res) => {
+    componentDidMount() {
+        const { id, wpnonce } = this.props.url.query;
+        fetch(
+            `${
+                Config.apiUrl
+            }/wp/v2/posts/${id}?_wpnonce=${wpnonce}`,
+            { credentials: "include" } // required for cookie nonce auth
+        )
+            .then(res => res.json())
+            .then(res => {
                 this.setState({
                     post: res
                 });
