df621da9875ae1c564e613111a9a94b0ed9c3c9b
Ryan Williams <ryan.williams@postlight.com>
2018-12-14 15:41:38 -0500

    chore: remove credentials from config.js

--- frontend/config.js
+++ frontend/config.js
@@ -1,5 +1,1 @@
-export const Config = {
-	apiUrl: 'http://localhost:8080/wp-json',
-	apiUsername: 'nedstark',
-	apiPassword: 'winteriscoming',
-}
+export const Config = { apiUrl: 'http://localhost:8080/wp-json' }
