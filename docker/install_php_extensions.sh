af886b63aaafddec9d43cff162b1c8eab9b14bfd
Alexi Akl <alexiakl@gmail.com>
2018-12-17 17:28:53 +0200

    feat: migrate to docker (#160)
    * feat: migrate to docker

---
+++ docker/install_php_extensions.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+sudo apt-get install -y php
+sudo apt-get install -y libapache2-mod-php
+sudo apt-get install -y php-fpm
+sudo apt-get install -y php-mysql
+sudo apt-get install -y php-memcached
+sudo apt-get install -y php-pear
+sudo apt-get install -y php-xml
+sudo apt-get install -y php-mbstring
+sudo apt-get install -y php-xdebug
+sudo apt-get install -y php-intl
+sudo apt-get install -y php-curl
+sudo apt-get install -y php-gd
+sudo apt-get install -y php-zip
+sudo apt-get install -y php-json
