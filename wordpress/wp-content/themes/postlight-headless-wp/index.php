5bf12517683f1021731db424d9465ea521b8fdaa
Ryan Williams <ryrowi@gmail.com>
2019-01-02 11:25:17 -0500

    fix: handle pages as well as CPTs in REST redirect

--- wordpress/wp-content/themes/postlight-headless-wp/index.php
+++ wordpress/wp-content/themes/postlight-headless-wp/index.php
@@ -1,6 +1,6 @@
 <?php
 // Redirect individual posts to the REST API endpoint
-if ( is_single() ) {
+if ( is_singular() ) {
     header( sprintf( 'Location: /wp-json/wp/v2/%s/%s', get_post_type_object( get_post_type() )->rest_base, get_post()->ID ) );
 } else {
     header( 'Location: /wp-json/' );
