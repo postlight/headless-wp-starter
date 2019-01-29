a34ef150abbbda3ec8f3c7662eff9c4c5bb1edaf
Ryan Williams <ryrowi@gmail.com>
2019-01-29 11:44:34 -0500

    chore: remove leftovers

--- frontend/config.js
+++ frontend/config.js
@@ -2,9 +2,9 @@ let wpUrl;
 
 // Use localhost for clientside requests & docker hostname for serverside.
 if (typeof window !== 'undefined') {
-	wpUrl = 'http://localhost:8080/wp-json';
+    wpUrl = 'http://localhost:8080/wp-json';
 } else {
-	wpUrl = 'http://wp-headless/wp-json';
+    wpUrl = 'http://wp-headless/wp-json';
 }
 
 export const Config = { apiUrl: wpUrl };
