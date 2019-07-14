a66b0193721a855d3481989b288bd9269b8ea6a8
Bible <ptang8@bus.illinois.edu>
2019-07-14 18:04:56 +0900

    fix: workaround for local

--- frontend/config.js
+++ frontend/config.js
@@ -1,7 +1,12 @@
 let wpUrl = `${process.env.WORDPRESS_URL}/wp-json`;
 
+// XXX: Workaround for local env.
 // If we're running on Docker, use the WordPress container hostname instead of localhost.
-if (process.env.HOME === '/home/node') {
+
+if (
+  process.env.HOME === '/home/node' &&
+  process.env.NODE_ENV !== 'production'
+) {
   wpUrl = 'http://wp-headless:8080/wp-json';
 }
 const Config = {
