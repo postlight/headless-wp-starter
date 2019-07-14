5cc99daa91c89a1b34b2abb4009eadfc3e38f962
Bible <ptang8@bus.illinois.edu>
2019-07-14 16:18:08 +0900

    fix: use internal network inside docker

--- frontend/config.js
+++ frontend/config.js
@@ -1,4 +1,9 @@
-const wpUrl = `${process.env.WORDPRESS_URL}/wp-json`;
+let wpUrl = `${process.env.WORDPRESS_URL}/wp-json`;
+
+// If we're running on Docker, use the WordPress container hostname instead of localhost.
+if (process.env.HOME === '/home/node') {
+  wpUrl = 'http://wp-headless:8080/wp-json';
+}
 const Config = {
   apiUrl: wpUrl,
   AUTH_TOKEN: 'auth-token',
