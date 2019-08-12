7398725721ff576b196a70feb1eb633c747ddaa9
Bible <ptang8@bus.illinois.edu>
2019-08-13 02:30:41 +0900

    feat: manually update articles

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -43,7 +43,7 @@ class Index extends Component {
 
       return { page, posts, pages };
     } catch (err) {
-      if (err.data.status === 403) {
+      if (err.data && err.data.status === 403) {
         tokenExpired();
       }
     }
@@ -62,7 +62,7 @@ class Index extends Component {
           this.setState({ id });
         })
         .catch(err => {
-          if (err.data.status === 403) {
+          if (err.data && err.data.status === 403) {
             tokenExpired();
           }
         });
