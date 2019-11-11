5c914f2f9364abc5121e25268a0f0b0779db68a7
Ryan Williams <modelm@users.noreply.github.com>
2019-11-11 17:12:32 -0500

    feat: new design: add Link menu items

--- frontend/components/Menu.js
+++ frontend/components/Menu.js
@@ -25,45 +25,68 @@ class Menu extends Component {
   render() {
     const { menu } = this.props;
     const { token, username } = this.state;
-    const menuItems = menu.items.map(item => {
-      if (item.object === 'custom') {
-        return (
-          <option
-            value={item.url}
-            key={item.ID}
-          >
-            {item.title}
-          </option>
-        );
-      }
-      const slug = getSlug(item.url);
-      const actualPage = item.object === 'category' ? 'category' : 'post';
-      return (
-        <option
-          value={`/${actualPage}?slug=${slug}&apiRoute=${item.object}`}
-          key={item.ID}
-        >
-          {item.title}
-        </option>
-      );
-    });
 
     return (
       <div className="menu">
-        <div className="brand">
-          <Link href="/">
-            <a className="starter-kit-logo">
-              <Logo width={48} height={32}/>
-              <div className="pl2">
-                WordPress + React<br/>
-                Starter Kit
-              </div>
-            </a>
-          </Link>
+        <div class="flex">
+          <div className="brand bb flex justify-center items-center w-100 w-25-l bn-l">
+            <Link href="/">
+              <a className="starter-kit-logo">
+                <Logo width={48} height={32}/>
+                <div className="pl2">
+                  WordPress + React<br/>
+                  Starter Kit
+                </div>
+              </a>
+            </Link>
+          </div>
+          <div className="links dn flex-l justify-around items-center w-75">
+            {menu.items.map(item => {
+              if (item.object === 'custom') {
+                return (
+                  <Link href={item.url} key={item.ID}>
+                    <a>{item.title}</a>
+                  </Link>
+                );
+              }
+              const slug = getSlug(item.url);
+              const actualPage = item.object === 'category' ? 'category' : 'post';
+              return (
+                <Link
+                  as={`/${item.object}/${slug}`}
+                  href={`/${actualPage}?slug=${slug}&apiRoute=${item.object}`}
+                  key={item.ID}
+                >
+                  <a>{item.title}</a>
+                </Link>
+              );
+            })}
+          </div>
         </div>
-        <div className="dropdown">
+        <div className="dropdown bb flex justify-center items-center dn-l">
           <select>
-            {menuItems}
+            {menu.items.map(item => {
+              if (item.object === 'custom') {
+                return (
+                  <option
+                    value={item.url}
+                    key={item.ID}
+                  >
+                    {item.title}
+                  </option>
+                );
+              }
+              const slug = getSlug(item.url);
+              const actualPage = item.object === 'category' ? 'category' : 'post';
+              return (
+                <option
+                  value={`/${actualPage}?slug=${slug}&apiRoute=${item.object}`}
+                  key={item.ID}
+                >
+                  {item.title}
+                </option>
+              );
+            })}
           </select>
         </div>
       </div>
