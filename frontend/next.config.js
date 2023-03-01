505bd857249c1a4ecb214446275937070e8f49af
Ezequiel Rangel <chekelinho@gmail.com>
2023-03-01 13:39:12 -0700

    Updates config files for Next 13

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,53 +1,11 @@
 const path = require('path');
-const glob = require('glob');
 
-module.exports = {
-  webpack: config => {
-    config.module.rules.push(
-      {
-        test: /\.(css|scss)/,
-        loader: 'emit-file-loader',
-        options: {
-          name: 'dist/[path][name].[ext]',
-        },
-      },
-      {
-        test: /\.css$/,
-        use: ['babel-loader', 'raw-loader', 'postcss-loader'],
-      },
-      {
-        test: /\.s(a|c)ss$/,
-        use: [
-          'babel-loader',
-          'raw-loader',
-          'postcss-loader',
-          {
-            loader: 'sass-loader',
-            options: {
-              includePaths: ['styles', 'node_modules']
-                .map(d => path.join(__dirname, d))
-                .map(g => glob.sync(g))
-                .reduce((a, c) => a.concat(c), []),
-            },
-          },
-        ],
-      },
-      {
-        test: /\.svg$/,
-        use: [
-          {
-            loader: 'react-svg-loader',
-            options: {
-              svgo: {
-                plugins: [
-                  { removeViewBox: false }
-                ],
-              }
-            }
-          },
-        ]
-      },
-    );
-    return config;
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'src/styles')],
   },
 };
+
+module.exports = nextConfig;
