67d721d925d1ed4364d0e8f29d0eadd5933f936b
Ezequiel Rangel <chekelinho@gmail.com>
2023-03-01 16:03:02 -0700

    Update rest of components

--- frontend/components/Layout.tsx
+++ frontend/components/Layout.tsx
@@ -1,6 +1,4 @@
-import React from 'react';
 import Header from './Header';
-import Menu from './Menu';
 import Footer from './Footer';
 
 const Layout = props => {
--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import Link from 'next/link';
 import Router from 'next/router';
 import WPAPI from 'wpapi';
